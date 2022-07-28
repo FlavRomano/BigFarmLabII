64cab56df6dc0cda0e3eb1c88166e2a126179262
Flavio Romano <flavio.romano92@gmail.com>
2022-07-28 10:00:14 +0200

    add-test-thing

--- client.c
+++ client.c
@@ -12,6 +12,53 @@
 #define HOST "127.0.0.1" /* local host */
 #define PORT 65201
 
+ssize_t readn(int fd, void *ptr, size_t n)
+{
+    size_t nleft;
+    ssize_t nread;
+
+    nleft = n;
+    while (nleft > 0)
+    {
+        if ((nread = read(fd, ptr, nleft)) < 0)
+        {
+            if (nleft == n)
+                return -1; /* error, return -1 */
+            else
+                break; /* error, return amount read so far */
+        }
+        else if (nread == 0)
+            break; /* EOF */
+        nleft -= nread;
+        ptr += nread;
+    }
+    return (n - nleft); /* return >= 0 */
+}
+
+/* Write "n" bytes to a descriptor */
+ssize_t writen(int fd, void *ptr, size_t n)
+{
+    size_t nleft;
+    ssize_t nwritten;
+
+    nleft = n;
+    while (nleft > 0)
+    {
+        if ((nwritten = write(fd, ptr, nleft)) < 0)
+        {
+            if (nleft == n)
+                return -1; /* error, return -1 */
+            else
+                break; /* error, return amount written so far */
+        }
+        else if (nwritten == 0)
+            break;
+        nleft -= nwritten;
+        ptr += nwritten;
+    }
+    return (n - nleft); /* return >= 0 */
+}
+
 void comunicazione(int event, long num)
 {
     int fd_skt;
@@ -46,6 +93,10 @@ void comunicazione(int event, long num)
     else
     {
         ;
+    }
+    if (close(fd_skt) < 0)
+    {
+        termina("Errore chiusura socket\n");
     }
 }
 
