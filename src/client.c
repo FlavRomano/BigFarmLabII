1166664d5093af8cf21354ac3d3f8c43dc81f6fc
Flavio Romano <flavio.romano92@gmail.com>
2022-11-10 18:02:51 +0100

    refactoring client

--- src/client.c
+++ src/client.c
@@ -1,39 +1,67 @@
-#include "/home/local/ADUNIPI/f.romano39/BigFarmLabII/include/apilab.h"
+#include "apilab.h"
 #include <stdbool.h>
 
 #define _GNU_SOURCE
 #define HOST "127.0.0.1" /* local host */
 #define PORT 65201
 
-typedef struct
+void ricezione(int fd_skt, int s_request)
 {
-    int fd_skt;
-    int type_server_request;
-} segment;
+    int e = readn(fd_skt, &s_request, sizeof(int));
+    if (e != sizeof(int))
+    {
+        termina("Errore read");
+    }
+    int n = ntohl(s_request);
+    char *server_response = malloc(n + 1);
+    int i;
+    for (i = 0; i < n; i++)
+    {
+        e = readn(fd_skt, &s_request, sizeof(int));
+        if (e != sizeof(int))
+        {
+            termina("Errore read");
+        }
+        char c = ntohl(s_request);
+        server_response[i] = c;
+    }
+    if (close(fd_skt) < 0)
+    {
+        termina("Errore chiusura socket");
+    }
+    server_response[i] = '\0';
+    printf("%s", server_response);
+    free(server_response);
+}
 
-segment *comunicazione(long l, bool richiesta_singola)
+void comunicazione(long l, bool request_all_pairs)
 {
-    int fd_skt;
+    int fd_skt = 0;
     struct sockaddr_in serv;
     size_t e;
-    fd_skt = 0;
-
     if ((fd_skt = socket(AF_INET, SOCK_STREAM, 0)) < 0)
     {
         termina("Errore creazione socket");
     }
-
     serv.sin_family = AF_INET;
     serv.sin_port = htons(PORT);
     serv.sin_addr.s_addr = inet_addr(HOST);
-
     if (connect(fd_skt, (struct sockaddr *)&serv, sizeof(serv)) < 0)
     {
         termina("Errore apertura connessione");
     }
 
     int s_request;
-    if (richiesta_singola)
+    if (request_all_pairs)
+    {
+        s_request = htonl(-1);
+        e = writen(fd_skt, &s_request, sizeof(int));
+        if (e != sizeof(int))
+        {
+            termina("Errore write");
+        }
+    }
+    else
     {
         size_t client_long_len = snprintf(NULL, 0, "%ld", l);
         char client_long[client_long_len];
@@ -54,68 +82,21 @@ segment *comunicazione(long l, bool richiesta_singola)
             }
         }
     }
-    else /* stampa di tutte le coppie "somma:file" */
-    {
-        s_request = htonl(-1);
-        e = writen(fd_skt, &s_request, sizeof(int));
-        if (e != sizeof(int))
-        {
-            termina("Errore write");
-        }
-    }
-    e = readn(fd_skt, &s_request, sizeof(int));
-    if (e != sizeof(int))
-    {
-        termina("Errore read");
-    }
-    segment *s = malloc(sizeof(segment));
-    s->fd_skt = fd_skt;
-    s->type_server_request = s_request;
-    return s;
-}
-
-void ricezione(segment *s)
-{
-    int fd_skt = s->fd_skt;
-    int s_request = s->type_server_request;
-    int n = ntohl(s_request);
-    char *server_response = malloc(n + 1);
-    int i, e;
-    for (i = 0; i < n; i++)
-    {
-        e = readn(fd_skt, &s_request, sizeof(int));
-        if (e != sizeof(int))
-        {
-            termina("Errore read");
-        }
-        char c = ntohl(s_request);
-        server_response[i] = c;
-    }
-    if (close(fd_skt) < 0)
-    {
-        termina("Errore chiusura socket");
-    }
-    server_response[i] = '\0';
-    printf("%s", server_response);
-    free(server_response);
-    free(s);
+    ricezione(fd_skt, s_request);
 }
 
 int main(int argc, char const *argv[])
 {
-    bool richiesta_singola = argc > 1;
-    if (richiesta_singola)
+    if (argc == 1)
     {
-        for (int i = 1; i < argc; i++)
-        {
-            segment *s = comunicazione(atol(argv[i]), true);
-            ricezione(s);
-        }
+        comunicazione(0, true);
     }
     else
     {
-        segment *s = comunicazione(0, false);
-        ricezione(s);
+        for (int i = 1; i < argc; i++)
+        {
+            comunicazione(atol(argv[i]), false);
+        }
     }
     return 0;
 }
