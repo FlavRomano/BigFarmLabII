88a9f4754eb4a0c153816dc9ef687602b0399aa8
Flavio Romano <flavio.romano92@gmail.com>
2022-11-24 16:11:38 +0100

    refactoring collector

--- bin/collector.py
+++ bin/collector.py
@@ -2,40 +2,11 @@
 
 import socket
 import struct
-import sys
 import threading
 from threading import Lock
 
 HOST = "127.0.0.1"
 PORT = 65201
-
-class ClientThread(threading.Thread):
-    def __init__(self,conn,addr,res, files):
-        threading.Thread.__init__(self)
-        self.conn = conn
-        self.addr = addr
-        self.res = res
-        self.files = files
-    def run(self):
-        mutex = Lock()
-        gestisci_connessione(self.conn, self.res, self.files, mutex)
-
-def main(host=HOST, port=PORT):
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        try:
-            s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) # for avoiding [Errno 98] Address already in use 
-            s.bind((host, port))
-            s.listen()
-            print("\t\t== Server attivo ==")
-            res = dict()
-            files = list()
-            while True:
-                conn, addr = s.accept()
-                t = ClientThread(conn, addr, res, files)
-                t.start()
-        except KeyboardInterrupt:
-            pass
-        s.shutdown(socket.SHUT_RDWR)
         
             
 def gestisci_connessione(conn, dic, files, mutex):
@@ -43,34 +14,32 @@ def gestisci_connessione(conn, dic, files, mutex):
         data = recv_all(conn, 4)
         dim = struct.unpack("!i", data[:4])[0]
         if dim > -1:
-            s = "".join([chr(struct.unpack("!i", recv_all(conn,4)[:4])[0]) for i in range(dim)])
+            s = "".join([chr(struct.unpack("!i", recv_all(conn, 4)[:4])[0]) for i in range(dim)])
             if ":" in s:
                 ricezione_farm(s, dic, files, mutex)
             else:
-                cerca_somma(s, conn, dic, mutex, 1)
+                cerca_somma(s, conn, dic, mutex)
         else:
             print_coppie(conn, dic, mutex)
     
 
 def ricezione_farm(s, dic, files, mutex):
-    arr = s.split(":")
-    name = arr[0]
-    l = arr[1]
+    farm_mess = s.split(":")
+    file_name = farm_mess[0]
+    long = farm_mess[1]
     mutex.acquire()
-    if l not in dic:
-        files.append(arr[0])
-        dic[arr[1]] = arr[0]
-    if l in dic and name not in files:
-        files.append(arr[0])
-        dic[arr[1]] += f", {arr[0]}" 
+    if long not in dic:
+        files.append(file_name)
+        dic[long] = file_name
+    if long in dic and file_name not in files:
+        files.append(file_name)
+        dic[long] += f", {file_name}" 
     mutex.release()
 
 
-def cerca_somma(s, conn, dic, mutex, flag):
+def cerca_somma(s, conn, dic, mutex):
     mutex.acquire()
-    stop = 0
-    if len(dic) == 0 or flag == 0:
-        stop = 1
+    if s not in dic:
         mess = f"{'Nessun file' : >12}\n"
         conn.sendall(struct.pack("!i", len(mess)))
         for c in mess:
@@ -80,13 +49,10 @@ def cerca_somma(s, conn, dic, mutex, flag):
             if k == s:
                 res = ""
                 res += (f"{k : >12} {dic.get(k)}\n")
-                stop = 1
                 conn.sendall(struct.pack("!i", len(res)))
                 for c in res:
                     conn.sendall(struct.pack("!i", ord(c)))
     mutex.release()
-    if stop == 0:
-        cerca_somma(s, conn, dic, mutex, 0)
 
 
 def print_coppie(conn, dic, mutex):
@@ -106,7 +72,7 @@ def print_coppie(conn, dic, mutex):
     mutex.release()
 
     
-def recv_all(conn,n): 
+def recv_all(conn, n): 
   chunks = b''
   bytes_recd = 0
   while bytes_recd < n: 
@@ -118,12 +84,35 @@ def recv_all(conn,n):
   return chunks
 
 
+class ClientThread(threading.Thread):
+    def __init__(self, conn, addr, res, files):
+        threading.Thread.__init__(self)
+        self.conn = conn
+        self.addr = addr
+        self.res = res
+        self.files = files
+    def run(self):
+        mutex = Lock()
+        gestisci_connessione(self.conn, self.res, self.files, mutex)
+
+
+def main(host=HOST, port=PORT):
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        try:
+            s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) # for avoiding [Errno 98] Address already in use 
+            s.bind((host, port))
+            s.listen()
+            print("\t\t== Server attivo ==")
+            res = dict()
+            files = list()
+            while True:
+                conn, addr = s.accept()
+                t = ClientThread(conn, addr, res, files)
+                t.start()
+        except KeyboardInterrupt:
+            pass
+        s.shutdown(socket.SHUT_RDWR)
+
+
 if __name__ == "__main__":
-    if len(sys.argv) == 1:
-        main()
-    elif len(sys.argv) == 2:
-        main(sys.argv[1])
-    elif len(sys.argv) == 3:
-        main(sys.argv[1], int(sys.argv[2]))
-    else:
-        print("Uso:\n\t %s [host] [port]" % sys.argv[0])
+    main()
